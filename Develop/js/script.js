aacdde854713c9985595bbbbe9cf998fadc8512b
Your NameAlex <alehr45@gmail.com>
2021-01-22 21:07:50 -0600

    added variables for every possible letter number or special character

---
+++ Develop/js/script.js
@@ -0,0 +1,7 @@
+var generateBtn = document.querySelector("#generate");
+
+var types = [];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var spChars = "`! @#$%^&*()_-+={[}]|:;'<,>.?/~"
+var numbers = "0123456789"
