0ff47b08818f2071a07b10bd4291e4dcffb682c2
Your NameAlex <alehr45@gmail.com>
2021-01-23 10:24:01 -0600

    added function for password length

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -5,3 +5,24 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var spChars = "`! @#$%^&*()_-+={[}]|:;'<,>.?/~"
 var numbers = "0123456789"
+
+function getRandomCharacter(characterType) {
+    return characterType[Math.floor(Math.random() * Math.floor(characterType.length))];
+  }
+  
+  function getRandomCharacter(characterType) {
+    return characterType[Math.floor(Math.random() * Math.floor(characterType.length))];
+  }
+  
+  
+  // Gather user input for the length of password
+  function lengthInput() {
+  
+    passwordLength = prompt("How many characters would you like your password?");
+  
+    if ((passwordLength < 8) || (passwordLength > 128) || (isNaN(passwordLength) === true)) {
+      alert("Please enter a number between 8-128")
+      lengthInput()
+    }
+  }
+  
