2c6f76e060093a5be037c18f4ac4473a501e7807
Your NameAlex <alehr45@gmail.com>
2021-01-23 14:31:25 -0600

    added function for user type length

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -15,7 +15,7 @@ function getRandomCharacter(characterType) {
   }
   
   
-  // Gather user input for the length of password
+
   function lengthInput() {
   
     passwordLength = prompt("How many characters would you like your password?");
@@ -26,3 +26,30 @@ function getRandomCharacter(characterType) {
     }
   }
   
+  function typeInput() {
+
+    
+    num = confirm("Would you like to add Numbers?")
+    lowCase = confirm("Would you like to add Lowercase Letters?")
+    upCase = confirm("Would you like to add Uppercase Letters?")
+    spChar = confirm("Would you like to add Special Characters?")
+  
+    
+    if ((num === false) && (lowCase === false) && (upCase === false) && (spChar === false)) {
+      alert("Please choose at least one character type")
+      typeInput();
+    }
+  
+    if (num === true) {
+      types.push(1)
+    }
+    if (lowCase === true) {
+      types.push(2)
+    }
+    if (upCase === true) {
+      types.push(3)
+    }
+    if (spChar === true) {
+      types.push(4)
+    }
+  }
